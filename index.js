7b319909ac85c7ae60d352bb9d2445655e99dabe
ahmadassi99 <ahmad.assi.2133@gmail.com>
2023-09-27 12:41:56 +0300

    "add commits"

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ const files=configPath.files;
 
 async function ProccessFile(filePath) {
     try {
+        //read the file,uft8 to read it as a text.
       const data = await readFile(filePath,'utf8');
       console.log(data);
+      //call wordcount to count words, i edited the method pattern to accept Special Characters.
       const wordCountResult = wordCount(data);
       //handel if file is empty
       if(!wordCountResult==0){
@@ -21,11 +23,12 @@ async function ProccessFile(filePath) {
       else{
         console.log(`${filePath} is Empty`);
       }
+      //here if the file is not existed.
     } catch (error) {
       console.error(`Got an error trying to read the file${filePath}: ${error.message}`);
    }
   }
-
+   //iterate the config.json file and process each file in it.
    files.forEach(file => {
     ProccessFile(file);
     
